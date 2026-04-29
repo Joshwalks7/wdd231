3b036308b2ea2e0d17825001a2239850e64ee539
Joshua Walker <jwalks7@byui.edu>
2026-04-29 09:35:35 -0600

    part 2

--- src/js/main.js
+++ src/js/main.js
@@ -6,3 +6,34 @@ function updateParkInformation() {
 }
 
 updateParkInformation();
+
+function addEventListeners() {
+    const menuTrigger = document.querySelector("#header-menu-trigger");
+    const menuOptions = document.querySelector("#header-menu-options");
+    const overview = document.querySelector("#parkInfo");
+
+    //MENU toggle feature
+    if (menuTrigger && menuOptions) {
+        menuTrigger.addEventListener("click", () => {
+            menuOptions.classList.toggle("is-hidden");
+        });
+        // event delegation (single listener)
+        menuOptions.addEventListener("click", (event) => {
+            const itemName = event.target.textContent;
+            console.log(itemName);
+        });
+    }
+
+    //overlay hover color toggle
+    if (overview) { //don't add these listeners if element not there
+        overview.addEventListener("mouseenter", () => {
+            overview.classList.add("overlay-hover");
+        });
+
+        overview.addEventListener("mouseleave", () => {
+            overview.classList.remove("overlay-hover");
+        });
+    }
+}
+
+addEventListeners();
