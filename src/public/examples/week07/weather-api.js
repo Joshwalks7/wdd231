b625c63a5a8dc23b2f71dba5027d14cbbac41d20
Joshua Walker <jwalks7@byui.edu>
2026-06-01 10:45:46 -0600

    in clas es modules

---
+++ src/public/examples/week07/weather-api.js
@@ -0,0 +1,11 @@
+// api.js
+export function getWeatherData() {
+    const data = {
+        location: "Rexburg",
+        conditions: "sunny",
+        icon: "&#9728",
+        temp: 50,
+        windspeed: 2
+    };
+    return data;
+}
